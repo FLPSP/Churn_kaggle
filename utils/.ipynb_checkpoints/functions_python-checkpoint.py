1767e12bb18d12fc7db039ad38fb02dfa77d5a98
Felipe Santos Pimentel <felipesantos.pimentel@hotmail.com>
2024-01-31 14:17:40 -0300

    Implement: criando uma função para retornar um dataframe com EDA basica

--- utils/.ipynb_checkpoints/functions_python-checkpoint.py
+++ utils/.ipynb_checkpoints/functions_python-checkpoint.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def data_basic_treatment (df: "pd.DataFrame")-> "pd.DataFrame":
     """
     Rename all to lower case letters, feature and data, and removing all blank spaces
@@ -27,4 +30,21 @@ def data_types (df: "pd.DataFrame") -> "pd.DataFrame":
     for col in str_columns:
         df[col] = df[col].astype("str")
     
+    return df
+
+def basic_eda(df):
+    """
+    Function to return dataframe with summarized result containing n total values, n unique values and missing percent in each feature.
+    """
+    summary_data = []
+    for col in df.columns:
+        # calculate n total values, n unique values and missing percent in each feature
+        ntotal_values = df[col].count()
+        nunique_values = df[col].nunique()
+        missing_percent = ((df[col].isnull().sum() + df[col][df[col] == ""].sum()) / ntotal_values) *100
+        summary_data.append((col, nunique_values, missing_percent, ntotal_values))
+
+    # create DataFrame with results
+    df = pd.DataFrame(summary_data, columns = ["feature", "nunique_values", "missing_percent", "ntotal_values"])
+
     return df
